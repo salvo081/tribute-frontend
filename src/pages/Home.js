35b61de39cfa54c6fd791c63786117cbdad0328e
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-02 17:51:52 +0200

    home.js edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,26 @@
 import './Home.css';
+import AllBlogs from '../components/AllBlogs';
+import AllBoxes from '../components/AllBoxes';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import ScrollingText from '../components/ScrollingText';
+import Hero from '../components/Hero';
 
-export default function Home() {
-  return <>{/* <h2 className="Home-title">Home</h2> */}</>;
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <Navbar />
+        <Hero />
+      </header>
+      <main className="Home-main">
+        <AllBoxes />
+        <ScrollingText />
+        <AllBlogs />
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
+export default Home;
