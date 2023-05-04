91a410207db595939211563ff70f25c2981bf268
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-04 16:10:20 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,58 +7,22 @@ export default function Footer() {
       <div className="footer-container">
         <div className="newsletter-wrapper">
           <h3>Subscribe to our newsletter</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
-            dolorum, reprehenderit sequi a sed labore quae? Quia beatae
-            obcaecati ducimus ab itaque laboriosam!{' '}
-          </p>
+          <p>No spam, we promise! Just informations about your legends. </p>
           <form action="">
-            <input type="email" />
+            <input type="email" placeholder="Your Email *" />
             <input type="checkbox" name="" id="" />
+            <label for="newsletter">
+              I accept the Pivacy Policy and subscrieb the newsletters
+            </label>
           </form>
         </div>
-        <div className="newsletter-info">
+        <div className="newsletter-info-wrapper">
           <h3>Subscribe to our newsletter</h3>
-          <p>
+          <p className="newletter-info-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
             eum totam quia ex dolorem et incidunt velit quaerat? Error esse sit
             alias iure totam atque numquam dolorem quos suscipit sint?
           </p>
-        </div>
-      </div>
-      <div className="social-footer">
-        <div className="nav-company">
-          <NavLink to="/">
-            <p>&copy; 2023 Tribute - All right reserved. </p>
-          </NavLink>
-        </div>
-        <nav className="social-footer-container">
-          <ul className="nav-links">
-            <li>
-              <NavLink to="/privacy">Privacy Policy</NavLink>
-            </li>
-            <li>
-              <NavLink to="/service">Terms of Service</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="social-icon wrapper">
-          <nav className="social-footer-container">
-            <ul className="nav-links">
-              <li>
-                <NavLink to="/privacy">Facebook Policy</NavLink>
-              </li>
-              <li>
-                <NavLink to="/service">Instagram</NavLink>
-              </li>
-              <li>
-                <NavLink to="/service">Twitter</NavLink>
-              </li>
-              <li>
-                <NavLink to="/service">LinkedIn</NavLink>
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
     </footer>
