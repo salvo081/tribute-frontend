b5296455e7d299202be86c9757598b299ff4c47d
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-05-10 13:10:58 +0200

    added comments to LogoMarquee.js

--- src/components/LogoMarquee.js
+++ src/components/LogoMarquee.js
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { gsap } from 'gsap';
+import { gsap } from 'gsap'; // Import the GSAP library
 import './LogoMarquee.css';
 
-const BlockWidth = 1440 / 6;
+const BlockWidth = 1440 / 7; // Width of each logo block
 
+// Component that processes the image array and creates marquee blocks
 const ProccessBlocks = ({ images }) => {
   const [marqueeBlocks, setMarqueeBlocks] = useState([]);
 
@@ -11,6 +12,7 @@ const ProccessBlocks = ({ images }) => {
     const _marqueeBlocks = [];
     let block = [];
     while (_marqueeBlocks.length < 7) {
+      // We want 7 blocks of logos
       for (let i = 0; i < images.length; i++) {
         block.push(images[i]);
         if (block.length === 1) {
@@ -23,21 +25,23 @@ const ProccessBlocks = ({ images }) => {
   }, [images]);
 
   if (marqueeBlocks.length === 0) {
+    // If there are no blocks, don't render the marquee
     return null;
   }
 
   return <Marquee marqueeBlocks={marqueeBlocks} />;
 };
 
+// Component that creates the marquee animation
 const Marquee = ({ marqueeBlocks }) => {
-  const marqueeElements = useRef([]);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const marqueeTween = useRef();
+  const marqueeElements = useRef([]); // Reference to the DOM elements
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth); // State to keep track of the screen width
+  const marqueeTween = useRef(); // Reference to the animation
 
-  const BannerWidth = BlockWidth * marqueeBlocks.length;
+  const BannerWidth = BlockWidth * marqueeBlocks.length; // Width of the entire banner
 
   useEffect(() => {
-    resizeHandler();
+    resizeHandler(); // Call resizeHandler to set the initial size
     window.addEventListener('resize', resizeHandler);
     return () => {
       window.removeEventListener('resize', resizeHandler);
@@ -46,18 +50,20 @@ const Marquee = ({ marqueeBlocks }) => {
   }, []);
 
   useEffect(() => {
-    marqueeInitialSet();
-    marqueeTween.current && marqueeTween.current.pause().kill();
+    marqueeInitialSet(); // Call the function to initialize the positions of the logos
+    marqueeTween.current && marqueeTween.current.pause().kill(); // Pause and kill any existing animation
     marqueeTween.current = gsap.to(marqueeElements.current, {
-      x: `-=${BannerWidth}`,
+      // Create the animation with GSAP
+      x: `-=${BannerWidth}`, // Move the logos to the left
       ease: 'none',
-      repeat: -1,
-      duration: 20,
-      rotation: 0.1,
+      repeat: -1, // Repeat the animation infinitely
+      duration: 20, // Duration of each loop
+      rotation: 0.1, // Add a slight rotation effect to the logos
       modifiers: {
         x: (x) => {
+          // Modify the x position of each logo
           return (
-            ((parseFloat(x) + BlockWidth * (marqueeBlocks.length - 1)) %
+            ((parseFloat(x) + BlockWidth * (marqueeBlocks.length - 1)) % // Calculate the new x position
               BannerWidth) +
             'px'
           );
@@ -68,6 +74,7 @@ const Marquee = ({ marqueeBlocks }) => {
 
   const marqueeInitialSet = () => {
     gsap.set(marqueeElements.current, {
+      // Set the initial position of each logo
       // xPercent: -100,
       x: function (index) {
         return BlockWidth * index;
@@ -76,14 +83,16 @@ const Marquee = ({ marqueeBlocks }) => {
   };
 
   const resizeHandler = () => {
-    gsap.set(marqueeElements.current, { clearProps: 'all' });
-    setScreenWidth(window.innerWidth);
+    gsap.set(marqueeElements.current, { clearProps: 'all' }); // Clear any existing properties
+    setScreenWidth(window.innerWidth); // Update the screen width state
   };
 
   const marqueeElementsRefHandler = (e, i) => {
+    // Callback function to save the references to the logos
     marqueeElements.current[i] = e;
   };
 
+  // This function maps over the marqueeBlocks array and returns an array of JSX elements
   const renderMarqueeElements = () => {
     return marqueeBlocks.map((e, i) => (
       <div
@@ -101,9 +110,11 @@ const Marquee = ({ marqueeBlocks }) => {
     <div
       className="logo-marquee-wrapper"
       onMouseEnter={() => {
+        // Pause the animation when the user hovers over the marquee
         marqueeTween.current.pause();
       }}
       onMouseLeave={() => {
+        // Resume the animation when the user stops hovering over the marquee
         marqueeTween.current.resume();
       }}>
       <div className="boxes">{renderMarqueeElements()}</div>
